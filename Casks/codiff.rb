199420356f897fa79a586d3258bec2450ba8cbcc
cpojer <christoph.pojer@gmail.com>
2026-09-16 09:39:22 +0900

    Update Codiff to 1.14.0

--- Casks/codiff.rb
+++ Casks/codiff.rb
@@ -1,6 +1,6 @@
 cask "codiff" do
-  version "1.13.0"
-  sha256 "1a756713f93e04beb6c4c1ceef9230a231a249c694ed0e7df41199cd9791000d"
+  version "1.14.0"
+  sha256 "dc316d945bffdf0fb149f639c08159ac75c98ae43e63885649e3eae3dff827f2"
 
   url "https://github.com/nkzw-tech/codiff/releases/download/v#{version}/Codiff-darwin-arm64-#{version}.zip"
   name "Codiff"
